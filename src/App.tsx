4e8ee02824cb02eafd953542a087a30f0ecf5c04
Ishita Isha <ishaivy406@gmail.com>
2026-02-03 10:00:00 +0530

    Set up base App component and React Router shell (25%)

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+﻿import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { ThemeProvider } from "@/contexts/ThemeContext";
+import DashboardPage from "./pages/DashboardPage";
+import NarrativeExplorerPage from "./pages/NarrativeExplorerPage";
+import ContradictionMapPage from "./pages/ContradictionMapPage";
+import TimelinePage from "./pages/TimelinePage";
+import ImpactInsightsPage from "./pages/ImpactInsightsPage";
+import SearchQueryPage from "./pages/SearchQueryPage";
+import StoryDetailPage from "./pages/StoryDetailPage";
